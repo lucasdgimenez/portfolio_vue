ff49e9251f486d5dec63f9424159696989d5bea9
lucasdgimenez <lucas_d360@hotmail.com>
2023-01-11 21:05:30 -0300

    ready 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,159 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!--<v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>-->
+
+    <!--<v-main>
+      <HelloWorld/>
+    </v-main>-->
+    <MainBackground/>
+
+    <section
+      class="mx-auto portfolio"
+    >
+    <!--<v-system-bar lights-out></v-system-bar>-->
+    <v-carousel
+      cycle
+      :continuous="true"
+      :interval="interval"
+      :show-arrows="true"
+      hide-delimiter-background
+      delimiter-icon="mdi-minus"
+      height="400"
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+        :style="{color: activeColor}"
+      >
+      {{slide.urlImg}}
+        <!--<v-sheet
+          :color="colors[i]"
+          height="100%"
+          tile
+        > :src="slide.src"-->
+
+        <v-row>
+          <v-btn @click="setRedirect(slide.urlLink)">Ir para o projeto</v-btn>
+        </v-row>
+          
+        <!--</v-sheet>-->
+      </v-carousel-item>
+    </v-carousel>
+    <!--<v-list two-line>
+      <v-list-item>
+      </v-list-item>
+    </v-list>-->
+    </section>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainBackground from './components/MainBackground';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    MainBackground,
+  },
+
+   data () {
+      return {
+        interval: 8000,
+        colors: [
+          'green',
+          'secondary',
+          'yellow darken-4',
+          'red lighten-2',
+          'orange darken-1',
+        ],
+        cycle: false,
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOTU5ODkyMTAwMDE4MCIsImp0aSI6ImUwMmI4YmUzLWJhZWQtNDdjNS1iMmI5LTgxODNjOWJkNzg5NiIsImlhdCI6IjEvMTEvMjAyMyA3OjUzOjI5IFBNIiwiaWR1c3VhcmlvIjoiMzMiLCJpZHVzdWFyaW9wYWkiOiIiLCJwZXJmaWwiOiIzIiwiaWRQZXNzb2FJNHBybyI6IjMxMjc2ODEiLCJub21lUGVyZmlsIjoiQ29ycmV0b3IiLCJub21lIjoiVk1WIENPUlJFVE9SQSBERSBTRUdVUk9TIExUREEiLCJsb2dpblVzdWFyaW8iOiIyOTU5ODkyMTAwMDE4MCIsImNvZGlnb0ZpbGlhbCI6IjEiLCJuclN1c2VwIjoiMTAyMDQ2ODkwMyIsImlkTGlzIjoiIiwicG9zc3VpQXNzZXNzb3JpYSI6IiIsIm5iZiI6MTY3MzQ2NjgwOSwiZXhwIjoxNjczNTUzMjA5LCJpc3MiOiJTb3VsSVRJc3N1ZXIiLCJhdWQiOiJTb3VsSVRBdWRpZW5jZSJ9.FQIHjm0DsQO4zGgH3kJhbZg1Zkatoa58gSApfGr0Qyg',
+        slides: [
+          {
+            nome: 'Seguro Residencial',
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            urlImg: './assets/img/PortalResidencial.PNG',
+            urlLink: 'http://150.136.201.214/PortalResidencial/#/'
+          }, 
+          {
+            nome: 'Seguro Vida',
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            urlImg: './assets/img/PortalResidencial.PNG',
+            //'PortalVarejo',
+            urlLink: 'http://150.136.201.214/PortalVarejo/#/'
+          },
+          {
+            nome: 'Seguro Vida Coletiva',
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            urlImg: './assets/img/PortalResidencial.PNG',
+            //'PortalVidaColetiva',
+            urlLink: 'http://150.136.201.214/PortalVidaColetiva/#/'
+          }
+        ],
+      }
+    },
+    methods: {
+      setRedirect(link) {
+        window.open(link+this.token, '_blank');
+      }
+    }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,
+ul,
+li,
+a {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
+.portfolio {
+  width: 70%;
 }
 </style>
---
+++ src/components/MainBackground.vue
@@ -0,0 +1,111 @@
+<template>
+  <section class="fullBackground">
+      <h2 class="titulo_main">Lucas Dias</h2>
+      <h3 class="sub_titulo">Front End</h3>
+      <nav class="menu_main">
+        <ul class="first">
+          <li><a href="#port" @click="teste">Portfolio</a></li>
+          <li><a href="#contact">Contato</a></li>
+        
+        </ul>
+        <ul class="second">
+            <li><a href="https://www.linkedin.com/in/lucasdgimenez/" target="_blanket">Linkedin</a></li>
+            <li><a href="https://github.com/lucasdgimenez" target="_blanket">GitHub</a></li>
+        </ul>
+      </nav>
+      
+        
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        teste() {
+            window.scroll({
+                top: 500,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+.fullBackground {
+    background-color: aqua;
+    background-position: center;
+    background-size: cover;
+    background-image: url('../assets/img/foto1.webp');
+    /*position: absolute;
+
+    top: 0;
+    left: 0;*/
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    color: white;
+}
+
+.menu_main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 400px;
+}
+
+.menu_main ul {
+  display: flex;
+  justify-content: center;
+}
+
+.menu_main a {
+  color: white;
+  background-color: #0168ba;
+  padding: 15px;
+}
+
+.menu_main ul.second {
+  margin: 60px 0 0 0;
+}
+
+.menu_main a:hover {
+  background-color: #090e53;
+  transition: 0.8s ease-out;
+}
+
+.menu_main li,
+.second_menu li {
+  margin: 0 8px;
+}
+
+.titulo_main,
+.sub_titulo {
+  text-transform: uppercase;
+  position: absolute;
+}
+
+.titulo_section {
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.titulo_main,
+.titulo_section {
+  font-size: 30pt;
+}
+
+.titulo_main {
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.sub_titulo {
+  font-size: 25pt;
+  top: 32%;
+  left: 42%;
+}
+
+</style>
